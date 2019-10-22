12811dac45f6f5654ce43afa2f6880d4bf6842c2
Andrés <ga.alaix564@gmail.com>
2019-10-22 13:31:50 -0500

    React Challenge 4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.mirror = this.mirror.bind(this);
+
+    this.state = {
+      mirror: ""
+    };
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
-      </div>
+        <input onChange={this.mirror.bind(this)} type="text" placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.mirror}</p>
+      </div >
     );
+  }
+  mirror(event) {
+    this.setState({
+      mirror: event.target.value
+    })
   }
 }
 
